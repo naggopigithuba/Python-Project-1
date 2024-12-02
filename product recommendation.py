184642875446b0e84aabe5254c3c29dc7f46b29f
Nagagopi Korne <111102209+naggopigithuba@users.noreply.github.com>
2024-12-02 11:22:16 +0530

    files

---
+++ product recommendation.py
@@ -0,0 +1,33 @@
+import numpy as np #importing numpy 
+
+users = ["user1","user2","user3"] # users  is a list
+products= ["laptop","mouse","monitor","pend rive","harddisk"] # list
+
+uids = {} #dict
+for i in range(len(users)): # start=0, stop 3 step 1  i 3 i<stop 3<3 True
+ uids[users[i]]=i # uids[users[0]]=0  user1: 0 user2:1 user3:2
+
+pids = {} # dict
+for i in range(len(products)): # start =0 stop =5 step =1 i 0 
+  pids[products[i]]=i # pids[laoptop]=0 , mouse 1 monitor 2 pendrive 3 harddisk 4
+
+print(uids, pids)
+
+prd_fq = np.zeros((len(users),len(products)))# 3*5 
+print(prd_fq)
+def reclist(uid): # function definition
+  uid = uids[uid]# Row id of User
+  upids = prd_fq[uid,:] # column id of the products
+  plist = np.argsort(upids)[::-1][:len(upids)] #frequnecy based sorting for the products
+  plist = [products[p]for p in plist] # to retrive items in the sorted order based on frequency
+  print(plist) #display
+
+def viewproduct(uid, pid):# function definition
+  uid = uids[uid] # row id of user
+  pid = pids[pid] # column id of the product
+  prd_fq[uid, pid] = prd_fq[uid, pid] + 1 # frequency increment
+  print(prd_fq) # display
+uname = input("Enter username:")
+print(reclist(uname))
+pname = input("Selectoneproduct:")
+viewproduct(uname,pname)
